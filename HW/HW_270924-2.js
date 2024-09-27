cae5ceb38e700c89be2316d59365d4bd718dfd0b
unknown <Wladimir334@github.com/Wladimir334/Mysite>
2024-09-27 12:55:50 +0300

    HW 270924

--- HW/HW_270924-2.js
+++ HW/HW_270924-2.js
@@ -1,86 +1,40 @@
-/*
-let fractions = {
-    a: 0,
-    b: 0,
-}
 
-function summation(s){
-    alert("Суммирование: " + (a/b + a/b))
-}
+    let A  = +prompt('Введите числитель1: '); 
+    let B  = +prompt('Введите знаменатель1: ');
+    let C  = +prompt('Введите числитель2: ');
+    let D  = +prompt('Введите знаменатель2: ');
 
-summation()
-*/
-/*
-function Fraction(numerator, denominator) {
-    this.numerator = numerator;
-    this.denominator = denominator;
-    }
-    
-    Fraction.prototype.add = function(otherFraction) {
-    let resultNumerator = this.numerator * otherFraction.denominator + otherFraction.numerator * this.denominator;
-    let resultDenominator = this.denominator * otherFraction.denominator;
-    return new Fraction(resultNumerator, resultDenominator);
-    }
-    
-    Fraction.prototype.subtract = function(otherFraction) {
-    let resultNumerator = this.numerator * otherFraction.denominator - otherFraction.numerator * this.denominator;
-    let resultDenominator = this.denominator * otherFraction.denominator;
-    return new Fraction(resultNumerator, resultDenominator);
-    }
-    
-    Fraction.prototype.multiply = function(otherFraction) {
-    let resultNumerator = this.numerator * otherFraction.numerator;
-    let resultDenominator = this.denominator * otherFraction.denominator;
-    return new Fraction(resultNumerator, resultDenominator);
-    }
-    
-    Fraction.prototype.divide = function(otherFraction) {
-    let resultNumerator = this.numerator * otherFraction.denominator;
-    let resultDenominator = this.denominator * otherFraction.numerator;
-    return new Fraction(resultNumerator, resultDenominator);
-    }
-    
-    Fraction.prototype.reduce = function() {
-    let gcd = function(a, b) {
-    return b === 0 ? a : gcd(b, a % b);
-    };
-    let commonDivisor = gcd(this.numerator, this.denominator);
-    return new Fraction(this.numerator / commonDivisor, this.denominator / commonDivisor);
-    }
-    
-    // Пример использования
-    let fraction1 = new Fraction(1, 2);
-    let fraction2 = new Fraction(1, 3);
-    
-    let sum = fraction1.add(fraction2);
-    console.log(sum);
-    
-    let difference = fraction1.subtract(fraction2);
-    console.log(difference);
-    
-    let product = fraction1.multiply(fraction2);
-    console.log(product);
-    
-    let quotient = fraction1.divide(fraction2);
-    console.log(quotient);
-    
-    let reducedFraction = fraction1.reduce();
-    console.log(reducedFraction);
+    let FrAdd = {}
+    function Add(FrAdd) {
+        addition = ((A/B) + (C/D));
+        const rounded = (addition).toFixed(2);
+        alert("Сложение: " + rounded);
 
-    */
-    let num1 = 1;
-    let den1 = 5;
-    let num2 = 3;
-    let den2 = 15;
-    let commonDen = den1 * den2;
-    let newNum1 = num1 * (commonDen / den1);
-    let newNum2 = num2 * (commonDen / den2);
-    let res = newNum1 + newNum2;
-    let gcd = function (a, b) {
-        return b === 0 ? a : gcd(b, a % b);
-    };
-    let div = gcd(res, commonDen);
-    res /= div;
-    commonDen /= div;
-    console.log(`${num1}/${den1} + ${num2}/${den2} is equal to ${res}/${commonDen}`);
+    }
     
+    let FrSub = {}
+    function Sub(FrSub) {
+        subtraction = ((A/B) - (C/D));
+        const rounded = (subtraction).toFixed(2);
+        alert("Вычитание: " + rounded);
+    }
+
+    let FrMul = {}
+    function Mul(FrMul) {
+        multiplication = ((A/B) * (C/D));
+        const rounded = (multiplication).toFixed(2);
+        alert("Умножение: " + rounded);
+    }
+
+    let FrDiv = {}
+    function Div(FrDiv) {
+        division = ((A/B) / (C/D));
+        const rounded = (division).toFixed(2);
+        alert("Деление: " + rounded);
+    }
+
+    Add(FrAdd);
+    Sub(FrSub);
+    Mul(FrMul);
+    Div(FrDiv);
+
--- HW/HW_270924-3.js
+++ HW/HW_270924-3.js
@@ -1,47 +1,55 @@
 
 
 
-let time = {};
 
+let time = {}
 function setTime(time) {
 let hours = +prompt('Enter hours: ');
 time.hours = hours;
 let minutes = +prompt('Enter minutes: ');
 time.minutes = minutes;
-let seconds = +prompt('Enter seconds: ');
-time.seconds = seconds;
+let second = +prompt('Enter seconds: ');
+time.second = second;
 };
 
 setTime(time);
-alert(`${time.hours} : ${time.minutes} : ${time.seconds}`);   
-
- /* function displayTime() {
-    alert(`${time.hours}:${time.minutes}:${time.seconds}`);
-}; */
+alert(`${time.hours} : ${time.minutes} : ${time.second}`);   
 
 
 let seconds = {};
 function addSeconds(seconds) {
-    let seconds = +prompt('Enter additional seconds: ');
-    time.seconds = seconds;
-    let totalSeconds = time.hours * 3600 + time.minutes * 60 + time.seconds + seconds;
+    let second = +prompt('Enter additional seconds: ');
+    time.second = second;
+    let totalSeconds = time.hours * 3600 + time.minutes * 60 + time.second + second;
     time.hours = Math.floor(totalSeconds / 3600) % 24;
     time.minutes = Math.floor(totalSeconds / 60) % 60;
-    time.seconds = totalSeconds % 60;
+    time.second = totalSeconds % 60;
 };
 
 addSeconds(seconds);
-alert(`${time.hours} : ${time.minutes} : ${time.seconds}`);
+alert(`${time.hours} : ${time.minutes} : ${time.second}`);
 
+let minutes = {}
 function addMinutes(minutes) {
-    let totalMinutes = time.hours * 60 + time.minutes + minutes;
+    let minute = +prompt('Enter additional minutes: ');
+    time.minute = minute;
+    let totalMinutes = time.hours * 60 + time.minute + minute;
     time.hours = Math.floor(totalMinutes / 60) % 24;
-    time.minutes = totalMinutes % 60;
+    time.minute = totalMinutes % 60;
 };
 
+addMinutes(minutes)
+alert(`${time.hours} : ${time.minute} : ${time.second}`);
+
+
+let hours = {}
 function addHours(hours) {
-    time.hours = (time.hours + hours) % 24;
+    let hour = +prompt('Enter additional hours: ');
+    time.hour = hour;
+    time.hour = (time.hour + hour) % 24;
 };
 
+addHours(hours)
+alert(`${time.hour} : ${time.minute} : ${time.second}`);
 
 

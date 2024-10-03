13a4be09640f51bb0acec4255a5166a3c1c55717
student <student@top.local>
2024-10-03 21:40:11 +0300

    031024

--- src/011024.js
+++
@@ -1,3 +0,0 @@
-let cars = ['mazda', 'toyota', 'bmw', 'mers', 'chevrolet'];
-for (let i = 0; i  < cars.length; i++);
-console.log(cars[i]);
---
+++ src/011024_031024.js
@@ -0,0 +1,5 @@
+/* let cars = ['mazda', 'toyota', 'bmw', 'mers', 'chevrolet'];
+for (let i = 0; i  < cars.length; i++);
+console.log(cars[i]);
+*/
+
